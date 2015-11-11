bc3cf7089b83609b6c950bb2d8d29ba49c2aa2ae
Tobias Schmidl <tobias@schmidl.cc>
2015-11-12 00:40:19 +0100

    Added proper syslog logging
    If we try to run this from `/etc/rc.local`, we need to be able to run headless...

--- check.py
+++ check.py
@@ -3,7 +3,20 @@
 import hashlib
 import json
 import os
-from sys import argv
+import logging
+import logging.handlers
+import sys
+import traceback
+
+#enable syslog logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+handler.setFormatter(logging.Formatter('%(module)s: %(levelname)s: %(message)s'))
+log.addHandler(handler)
+
+# write all uncaught exceptions to syslog
+sys.excepthook = lambda ex_cls, ex, tb: log.critical('{0}: {1} -- {2}'.format(ex_cls, ex, traceback.format_tb(tb)))
 
 def calculate_sha256_from_file(file_name, size = 0):
     block_size = 4096
@@ -35,9 +48,9 @@ def calc_sha256_on_luksHeader(device_name):
 if __name__ == '__main__':
     key_file_name = None
     try:
-        key_file_name = argv[1]
+        key_file_name = sys.argv[1]
     except IndexError:
-        print "Usage: " + argv[0] + " <key file json>"
+        print "Usage: " + sys.argv[0] + " <key file json>"
         exit(1)
 
     json_data = None
@@ -52,13 +65,13 @@ if __name__ == '__main__':
             actual_sum = calc_sha256_on_luksHeader(device_name)
             if desired_sum == None:
                 device['luksHeader'] = actual_sum
-                print(u"○ Updating configuration file: LUKS header for " + device_name + ": " + actual_sum[:8] + u"…")
+                log.notice(u"○ Updating configuration file: LUKS header for " + device_name + ": " + actual_sum[:8] + u"…")
                 data_has_changed = True
             elif desired_sum <> actual_sum:
-                print(u"✗ LUKS header for " + device_name + " failed: Should be " + desired_sum[:8] + u"…" + ", but really is " + actual_sum[:8] + u"…")
+                log.error(u"✗ LUKS header for " + device_name + " failed: Should be " + desired_sum[:8] + u"…" + ", but really is " + actual_sum[:8] + u"…")
                 exitCode = 1
             else:
-                print(u"✓ LUKS header for " + device_name + " correct: " + actual_sum[:8] + u"…")
+                log.info(u"✓ LUKS header for " + device_name + " correct: " + actual_sum[:8] + u"…")
         if device.has_key("sha256"):
             desired_sum = device['sha256']
             if device.has_key('size'):
@@ -68,16 +81,16 @@ if __name__ == '__main__':
             actual_sum = calculate_sha256_from_file(device_name, size)
             if desired_sum == None:
                 device['sha256'] = actual_sum
-                print(u"○ Updating configuration file: SHA sum for " + device_name + ": " + actual_sum[:8] + u"…")
+                log.notice(u"○ Updating configuration file: SHA sum for " + device_name + ": " + actual_sum[:8] + u"…")
                 data_has_changed = True
             elif desired_sum <> actual_sum:
-                print(u"✗ SHA sum for " + device_name + " failed: Should be " + desired_sum[:8] + u"…" + ", but really is " + actual_sum[:8] + u"…")
+                log.error(u"✗ SHA sum for " + device_name + " failed: Should be " + desired_sum[:8] + u"…" + ", but really is " + actual_sum[:8] + u"…")
                 exitCode = 1
             else:
-                print(u"✓ SHA sum for " + device_name + " correct: " + actual_sum[:8] + u"…")
+                log.info(u"✓ SHA sum for " + device_name + " correct: " + actual_sum[:8] + u"…")
 
     if data_has_changed:
-        print("Checksums have changed, updating key file")
+        log.notice("Checksums have changed, updating key file")
         with open(key_file_name, 'w') as text_file:
             json.dump(json_data, text_file)
 
